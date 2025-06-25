1097d838b7a152de16654618fa32e14dfed2851b
Colin Harwood <colinharwood0@gmail.com>
2025-06-25 18:33:34 +0200

    Added the allModules variable which holds all of the module times

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "functions.h"
+#include "module_data.h"
 
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 int main() {
-
+    cout << allModules[0].name << std::endl;
+    cout << allModules[0].lectures[0][1].startTime << std::endl;
 
     return 0;
 }
---
+++ module_data.h
@@ -0,0 +1,8 @@
+#ifndef MODULE_DATA_H
+#define MODULE_DATA_H
+
+#include "functions.h"
+
+extern module allModules[7];
+
+#endif
